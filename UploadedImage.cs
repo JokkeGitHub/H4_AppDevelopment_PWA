d7a75b2e2b244f84b94f0a4abdccead90248c876
Joachim Hansen <skraldespand94@hotmail.com>
2022-11-20 22:52:41 +0100

    updates

---
+++ UploadedImage.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace H4_AppDevelopment_PWA
+{
+    public class UploadedImage
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Base64Data { get; set; }
+        public long Size { get; set; }
+        public IBrowserFile File { get; set; }
+    }
+}
